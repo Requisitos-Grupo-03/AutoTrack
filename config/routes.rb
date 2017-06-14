4db5809fa4abc4a5cc0a5c0b1228c407a8b22ea8
MatheusRich <matheusrichardt@gmail.com>
2017-06-13 22:36:46 -0300

    Adding static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :members
   devise_for :installs
+
   resources :users
-    root 'users#index'
+  get '/home', to: 'static_pages#home_page'
+  # devise_scope :member do
+  #   root to: "devise/sessions#new"
+  # end
+    root 'static_pages#home_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
