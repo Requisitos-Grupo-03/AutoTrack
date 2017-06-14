8377496b89e3b7471df2c612c288cf355eb43d07
MatheusRich <matheusrichardt@gmail.com>
2017-06-14 11:37:26 -0300

    Adding not found page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get '/whatever', to: 'users#index'
   get '/home', to: 'static_pages#home_page'
   root 'static_pages#home_page'
+  get '*unmatched_route', :to => 'static_pages#not_found'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
