41003a7c088a385b2938d011c875e859bf50157c
ThalissonMelo <tallisonmelo@hotmail.com>
2017-06-14 11:54:37 -0300

    Structuring Kanban

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,25 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
+
+  # GET /boards
+  # GET /boards.json
+  def index
+    @boards = Board.all
+  end
+
+  # GET /boards/1
+  # GET /boards/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_board
+      @board = Board.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def board_params
+      params.require(:board).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :boards
+    root 'users#index'
   devise_for :members
   devise_for :installs
   resources :users
